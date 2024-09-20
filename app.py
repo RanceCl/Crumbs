48431b23ba9fd94fbaac06a8ddfa32b35908ded6
RanceCL <roderickrwhite2022@gmail.com>
2024-09-20 17:31:40 -0400

    Config added for CORS.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app)
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5173"}})
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
