657dc732e238811aaaf0d05e309a0a41294ffc0a
RanceCL <roderickrwhite2022@gmail.com>
2024-09-26 17:20:47 -0400

    Added endpoints for order manipulation for customers. Added a method to delete the currently logged in user. Fixed code so that deletion of a parent table entry will delete its (I'm not making up the name, this is what the documentation calls it) orphans.

--- models.py
+++ models.py
@@ -10,8 +10,8 @@ class Users(db.Model, UserMixin):
     password_hash = db.Column(db.String, nullable=False)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
-    cookies = db.relationship('Cookie_Inventory', back_populates = 'users')
-    customers = db.relationship('Customers', back_populates = 'users')
+    cookies = db.relationship('Cookie_Inventory', back_populates = 'users', cascade="all, delete-orphan")
+    customers = db.relationship('Customers', back_populates = 'users', cascade="all, delete-orphan")
 
     def set_email(self, email):
         # Make sure that email is in a valid format.
@@ -67,8 +67,8 @@ class Cookies(db.Model):
     description = db.Column(db.String, nullable=False, default='')
     price = db.Column(db.String, nullable=False, default=0)
     picture_url = db.Column(db.String, nullable=False, default='')
-    users = db.relationship('Cookie_Inventory', back_populates = 'cookies')
-    orders = db.relationship('Cookie_Orders', back_populates = 'cookies')
+    users = db.relationship('Cookie_Inventory', back_populates = 'cookies', cascade="all, delete-orphan")
+    orders = db.relationship('Cookie_Orders', back_populates = 'cookies', cascade="all, delete-orphan")
 
 class Customers(db.Model):
     __tablename__ = 'customers'
@@ -77,8 +77,8 @@ class Customers(db.Model):
     last_name = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    users = db.relationship('Users', back_populates='customers', cascade="all, delete")
-    orders = db.relationship('Orders', back_populates='customers')
+    users = db.relationship('Users', back_populates='customers')
+    orders = db.relationship('Orders', back_populates='customers', cascade="all, delete-orphan")
     
     def __init__(self, first_name, last_name, user_id):
         self.first_name = first_name
@@ -93,8 +93,8 @@ class Orders(db.Model):
     payment_id = db.Column(db.Integer)
     cost = db.Column(db.Integer, nullable=False, default=0)
     
-    customers = db.relationship('Customers', back_populates='orders', cascade="all, delete")
-    cookies = db.relationship('Cookie_Orders', back_populates = 'orders', cascade="all, delete")
+    customers = db.relationship('Customers', back_populates='orders')
+    cookies = db.relationship('Cookie_Orders', back_populates = 'orders', cascade="all, delete-orphan")
 
     def __init__(self, customer_id, payment_id):
         self.customer_id = customer_id
@@ -106,8 +106,8 @@ class Cookie_Inventory(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     cookie_id = db.Column(db.SmallInteger, db.ForeignKey('cookies.id'), primary_key=True)
     inventory = db.Column(db.Integer, nullable=False, default=0)
-    users = db.relationship('Users', back_populates='cookies', cascade="all, delete")
-    cookies = db.relationship('Cookies', back_populates = 'users', cascade="all, delete")
+    users = db.relationship('Users', back_populates='cookies')
+    cookies = db.relationship('Cookies', back_populates = 'users')
 
     def __init__(self, user_id, cookie_id, inventory):
         self.user_id = user_id
@@ -120,8 +120,8 @@ class Cookie_Orders(db.Model):
     cookie_id = db.Column(db.Integer, db.ForeignKey('cookies.id'), primary_key=True)
     quantity = db.Column(db.Integer, nullable=False, default=0)
     cost = db.Column(db.Integer, nullable=False, default=0)
-    orders = db.relationship('Orders', back_populates='cookies', cascade="all, delete")
-    cookies = db.relationship('Cookies', back_populates = 'orders', cascade="all, delete")
+    orders = db.relationship('Orders', back_populates='cookies')
+    cookies = db.relationship('Cookies', back_populates = 'orders')
     
     def __init__(self, order_id, cookie_id):
         self.order_id = order_id
--- routes.py
+++ routes.py
@@ -84,6 +84,28 @@ def logout():
     logout_user()
     return "Logged Out", 200
 
+
+# Delete customers based on id.
+@app.route('/delete_account', methods=['DELETE'])
+@login_required
+def delete_user():
+    if ('password' in request.form):
+        # Retreive and verify password
+        password = request.form.get("password")
+        if not password: 
+            return "Please enter current password to confirm change."
+        if not current_user.check_password(password):
+            return "Password is incorrect. Please try again."
+        user = Users.query.get(current_user.id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            logout_user
+            return "Your account has been deleted! :D"
+        return jsonify({'status': 'error', 'message': 'An account with this id has not been found.'}), 400
+        
+    return jsonify({'status': 'error', 'message': 'Please fill out the form!'}), 400
+
 '''
 @app.route('/users', methods=['POST'])
 @login_required
@@ -223,7 +245,7 @@ def add_customer():
 def read_customer(id):
     customer = Customers.query.filter_by(id=id, user_id=current_user.id).first()
     if not customer:
-        return "I'm sorry, " + id + " doesn't exist. :("
+        return "I'm sorry, a customer " + id + " doesn't belong to you. :("
     return {'id': customer.id, 
             'first_name': customer.first_name, 
             'last_name': customer.last_name, 
@@ -237,7 +259,7 @@ def update_customer(id):
         and 'last_name' in request.form):
         customer = Customers.query.filter_by(id=id, user_id=current_user.id).first()
         if not customer:
-            return "I'm sorry, " + id + " doesn't exist. :("
+            return "I'm sorry, a customer " + id + " doesn't belong to you. :("
         customer.first_name =request.form.get("first_name")
         customer.last_name =request.form.get("last_name")
         db.session.commit()
@@ -256,6 +278,155 @@ def delete_customer(id):
     return id + " deleted! :D"
 
 
+# Show customer orders based on id.
+@app.route('/customers/<id>/orders', methods=['GET'])
+@login_required
+def read_customer_orders(id):
+    #orders = Orders.query.join(Customers).filter(Customers.user_id==current_user.id, Orders.id==id).first()
+    orders = Orders.query.join(Customers).filter_by(id=id).all()
+    result = []
+    for order in orders:
+        result.append({
+            "id": order.id,
+            "customer_id": order.customer_id,
+            "customer_first_name": order.customers.first_name,
+            "customer_last_name": order.customers.last_name,
+            'user_id': order.customers.user_id,
+            'payment_id': order.payment_id
+        })
+    return {"orders": result}, 200
+
+# Add order to customer
+@app.route('/customers/<id>/orders', methods=['POST', 'PATCH'])
+@login_required
+def add_customer_order(id):
+    if ('payment_id' in request.form):
+        payment_id = request.form.get("payment_id")
+        order = Orders(customer_id=id,payment_id=payment_id)
+        db.session.add(order)
+        db.session.commit()
+        return {'id': order.id, 
+                'customer': order.customer_id, 
+                'payment': order.payment_id, 
+                'cost': order.cost}
+    return "Please fill out the form!"
+
+# Show customer order based on id.
+@app.route('/customers/<id>/orders/<order_id>', methods=['GET'])
+@login_required
+def read_customer_order(id, order_id):
+    order = Orders.query.join(Customers).filter(Customers.id==id, Orders.id==order_id).first()
+    if not order:
+        return "I'm sorry, customer " + id + " doesn't have an order with id " + order_id + ". :("
+    return {'id': order.id, 
+            'customer': order.customer_id, 
+            'payment': order.payment_id, 
+            'cost': order.cost}
+
+# Update customer orders based on id.
+@app.route('/customers/<id>/orders/<order_id>', methods=['PATCH'])
+@login_required
+def update_customer_order(id, order_id):
+    if ('payment_id' in request.form):
+        order = Orders.query.join(Customers).filter(Customers.id==id, Orders.id==order_id).first()
+        if not order:
+            return "I'm sorry, customer " + id + " doesn't have an order with id " + order_id + ". :("
+        order.payment_id = request.form.get("payment_id")
+        db.session.commit()
+        return {'id': order.id, 
+                'customer': order.customer_id, 
+                'payment': order.payment_id, 
+                'cost': order.cost}
+    return "Please fill out the form!"
+
+# Delete customer orders based on id.
+@app.route('/customers/<id>/orders/<order_id>', methods=['DELETE'])
+@login_required
+def delete_customer_order(id, order_id):
+    Orders.query.filter_by(id=order_id).delete()
+    db.session.commit()
+    return order_id + " deleted! :D"
+
+# ----------------------------------------- Orders -----------------------------------------
+# Retrieve orders 
+@app.route('/orders', methods=['GET'])
+@login_required
+def get_orders_list():
+    # inventory = db.session.query(Cookies.cookie_name, Cookie_Inventory.inventory).join(Customers).filter(Customers.user_id == current_user.id).all()
+    # inventory = db.session.query(Cookies.cookie_name, Cookie_Inventory.inventory).join(Cookie_Inventory).filter(Cookie_Inventory.user_id == current_user.id).all()
+    
+    orders = Orders.query.join(Customers).filter_by(user_id=current_user.id).all()
+    result = []
+    for order in orders:
+        result.append({
+            "id": order.id,
+            "customer_id": order.customer_id,
+            "customer_first_name": order.customers.first_name,
+            "customer_last_name": order.customers.last_name,
+            'user_id': order.customers.user_id,
+            'payment_id': order.payment_id
+        })
+    return {"orders": result}, 200
+
+# Add order from order page
+@app.route('/orders', methods=['POST', 'PATCH'])
+@login_required
+def add_order():
+    if ('customer_id' in request.form
+        and 'payment_id' in request.form):
+        customer_id = request.form.get("customer_id")
+        payment_id = request.form.get("payment_id")
+        customer = Customers.query.filter_by(id=id).first()
+        # Make sure customer exists before making an order for them.
+        if not customer:
+            return "I'm sorry, customer with" + id + " doesn't exist. Please create a customer and try again."
+        
+        order = Orders(customer_id=customer_id,payment_id=payment_id)
+        db.session.add(order)
+        db.session.commit()
+        return {'id': order.id, 
+                'customer': order.customer_id, 
+                'payment': order.payment_id, 
+                'cost': order.cost}
+    return "Please fill out the form!"
+
+# Show orders based on id.
+@app.route('/orders/<id>', methods=['GET'])
+@login_required
+def read_order(id):
+    order = Orders.query.join(Customers).filter(Orders.id==id).first()
+    if not order:
+        return "I'm sorry, you don't have an order with id " + id + ". :("
+    return {'id': order.id, 
+            'customer': order.customer_id, 
+            'payment': order.payment_id, 
+            'cost': order.cost}
+
+# Update orders based on id.
+@app.route('/orders/<id>', methods=['PATCH'])
+@login_required
+def update_order(id):
+    if ('customer_id' in request.form
+        and 'payment_id' in request.form):
+        order = Orders.query.join(Customers).filter(Orders.id==id).first()
+        if not order:
+            return "I'm sorry, you don't have an order with id " + id + ". :("
+        order.customer_id = request.form.get("customer_id")
+        order.payment_id = request.form.get("payment_id")
+        db.session.commit()
+        return {'id': order.id, 
+                'customer': order.customer_id, 
+                'payment': order.payment_id, 
+                'cost': order.cost}
+    return "Please fill out the form!"
+
+# Delete orders based on id.
+@app.route('/orders/<id>', methods=['DELETE'])
+@login_required
+def delete_order(id):
+    Orders.query.filter_by(id=id).delete()
+    db.session.commit()
+    return id + " deleted! :D"
 # ----------------------------------------- User Info -----------------------------------------
 # Show account based on id.
 @app.route('/users', methods=['GET'])
@@ -340,21 +511,24 @@ def change_password():
         return "Please fill out the form!"
     return "What you getting at?"
 
-'''
-# Delete account.
-@app.route('/users/<id>', methods=['DELETE'])
-@login_required
-def delete():
-    return "User deleted!"
-'''
+
 # ----------------------------------------- Dev Tests -----------------------------------------
 def add_user(email, password, first_name, last_name):
     password_confirm = password
-    new_user = Users(email=email, first_name=first_name, last_name=last_name)
+    new_user = Users(email=email, 
+                     first_name=first_name, 
+                     last_name=last_name)
     if not new_user.set_password(password, password_confirm):
         db.session.add(new_user)
         db.session.commit()
     return None
+
+def add_order(customer_id, payment_id):
+    new_order = Orders(customer_id=customer_id,
+                       payment_id=payment_id)
+    db.session.add(new_order)
+    db.session.commit()
+    return new_order.id
 
 def add_customer(first_name, user_id):
     new_customer = Customers(first_name=first_name,
@@ -363,11 +537,10 @@ def add_customer(first_name, user_id):
     #current_user.append(new_customer)
     db.session.add(new_customer)
     db.session.commit()
-    return None
+    return new_customer.id
 
 @app.route('/populate_users', methods=['GET','POST'])
 def populate_users():
-    Users.query.delete()
     add_user("chadgregpaulthompson@gmail.com", "Ch@t3PT", "Chad", "GPT")
     add_user("anonymous@email.com", "S3cr3t P@$$word", "Anonymous", "NoLastName")
     add_user("known@email.com", "S33n P@$$word", "Known", "HasLastName")
@@ -377,24 +550,26 @@ def populate_users():
     return "Table populated"
     # return redirect(url_for('login'))
 
+# For each customer, add four orders
+def populate_orders(customer_id):
+    for _ in range(4):
+        add_order(customer_id, 0)
+    return None
+
 @app.route('/populate_customers', methods=['GET','POST'])
 def populate_customers():
-    Customers.query.delete()
-    add_customer("Chad", 1)
-    add_customer("Jim", 2)
-    add_customer("Known", 3)
-    add_customer("Jane", 4)
-    add_customer("Don", 1)
-    add_customer("Abe", 2)
-    add_customer("Corey", 3)
-    add_customer("Linda", 4)
-    add_customer("Leen", 1)
-    add_customer("Noni", 2)
-    add_customer("Chi-Chi", 3)
-    add_customer("Best", 4)
-    add_customer("Midnight", 1)
-    add_customer("Marley", 2)
-    add_customer("Known", 3)
-    add_customer("Chad", 4)
-
+    names = ["Chad","Jim","Known","Jane","Don","Abe","Corey","Linda","Leen","Noni","Chi-Chi","Best","Midnight","Marley","Known","Chad"]
+    name_index = 0
+    user_ids = [id[0] for id in Users.query.with_entities(Users.id).all()]
+    for user_id in user_ids:
+        # Add three names to the current user.
+        for _ in range(3):
+            if name_index >= len(names):
+                # Stop adding customers if we've used all of the names.
+                return "Customers populated"
+            
+            new_customer_id = add_customer(names[name_index], user_id)
+            populate_orders(new_customer_id)
+            name_index += 1
+            
     return "Customers populated"
