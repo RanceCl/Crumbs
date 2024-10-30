01265905ce6a89743bb5410c6691fe9abee655eb
RanceCL <roderickrwhite2022@gmail.com>
2024-10-30 19:23:17 -0400

    Jsonified return message for inventory changing.

--- app/inventory/routes.py
+++ app/inventory/routes.py
@@ -37,7 +37,7 @@ def set_user_inventory():
     if ('cookie_name' in data
         and 'inventory' in data):
         cookie_name = data.get("cookie_name")
-        inventory=int(data.get("inventory"))
+        inventory = data.get("inventory")
         cookie = Cookies.query.filter_by(cookie_name=cookie_name).first()
         
         # Ensure that the cookie exists.
@@ -50,10 +50,10 @@ def set_user_inventory():
             cookie_inventory = Cookie_Inventory(user_id=current_user.id, cookie_id=cookie.id, inventory=inventory)
             db.session.add(cookie_inventory)
             db.session.commit()
-            return jsonify(cookie_name + " added to inventory table! You have " + str(inventory) + " in stock! :D"), 200
+            return jsonify({"message": cookie_name + " added to inventory table! You have " + str(inventory) + " in stock! :D"}), 200
         cookie_inventory.inventory = inventory
         db.session.commit()
-        return jsonify(cookie_name + " inventory updated! You have " + str(cookie_inventory.projected_inventory) + " out of " + str(inventory) + " in stock! :D"), 200
+        return jsonify({"message": cookie_name + " inventory updated! You have " + str(cookie_inventory.projected_inventory) + " out of " + str(inventory) + " in stock! :D"}), 200
     return jsonify({'status': 'error', 'message': 'Please fill out the form!'}), 400
 
 # Delete inventory
