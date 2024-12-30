57ef8cffb29570b70142a2865eada5d274c56874
RanceCl <104018154+RanceCl@users.noreply.github.com>
2024-12-30 12:30:41 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,4 @@
 from app import create_app
-'''
-from flask import Flask
-from flask_login import LoginManager
-from flask_cors import CORS
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5173"}})
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-db = SQLAlchemy(app)
-
-if __name__ == '__main__':
-    from models import *
-    from routes import *
-    app.run(debug=True)
-'''
 
 app = create_app()
 
